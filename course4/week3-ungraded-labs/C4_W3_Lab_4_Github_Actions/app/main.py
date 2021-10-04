7343e19b6b6c3e9e54aa258e94bb5bec554c60d1
roman807 <roman.moser@gmx.net>
2021-10-04 06:00:01 +0200

    testing ci/cd pipeline

--- course4/week3-ungraded-labs/C4_W3_Lab_4_Github_Actions/app/main.py
+++ course4/week3-ungraded-labs/C4_W3_Lab_4_Github_Actions/app/main.py
@@ -4,7 +4,7 @@ from typing import List
 from fastapi import FastAPI
 from pydantic import BaseModel, conlist
 
-
+#random change to push
 
 app = FastAPI(title="Predicting Wine Class with batching")
 
